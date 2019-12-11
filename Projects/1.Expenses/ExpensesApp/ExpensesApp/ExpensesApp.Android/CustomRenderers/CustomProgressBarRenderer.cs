fb7d637642fa95b57efd95bbfa088b0d414b133d
Glareone <enlarius@gmail.com>
2019-12-11 13:17:46 +0300

    android custom renderer as well

--- Projects/1.Expenses/ExpensesApp/ExpensesApp/ExpensesApp.Android/CustomRenderers/CustomProgressBarRenderer.cs
+++ Projects/1.Expenses/ExpensesApp/ExpensesApp/ExpensesApp.Android/CustomRenderers/CustomProgressBarRenderer.cs
@@ -1,47 +1,42 @@
-﻿using CoreGraphics;
-using ExpensesApp.iOS.CustomRenderers;
+﻿using Android.Content;
 using Xamarin.Forms;
-using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms.Platform.Android;
+using ExpensesApp.Droid.CustomRenderers;
 
 // Necessary attribute
 [assembly: ExportRenderer(typeof(ProgressBar), typeof(CustomProgressBarRenderer))]
-namespace ExpensesApp.iOS.CustomRenderers
+namespace ExpensesApp.Droid.CustomRenderers
 {
     // Custom renderer which replace default renderer on CategoriesPage for IOS devices.
     // It makes progress bar a bit wider
     public class CustomProgressBarRenderer : ProgressBarRenderer
     {
+        public CustomProgressBarRenderer(Context context) : base(context)
+        {
+        }
+
         protected override void OnElementChanged(ElementChangedEventArgs<ProgressBar> e)
         {
             base.OnElementChanged(e);
 
             // Change the progress bar color is it's nan.
             // and little differentiation by colors
+
+            // pay attention. Control here has a different type and different methods rather than Control from IOS Renderer
             if (double.IsNaN(e.NewElement.Progress))
             {
-                Control.ProgressTintColor = Color.FromHex("#00B9AE").ToUIColor();
+                Control.ProgressDrawable.SetTint(Color.FromHex("#00B9AE").ToAndroid());
             }
             else if (e.NewElement.Progress < 0.3)
             {
-                Control.ProgressTintColor = Color.FromHex("#00BDD5").ToUIColor();
+                Control.ProgressDrawable.SetTint(Color.FromHex("#00BDD5").ToAndroid());
             }
             else if (e.NewElement.Progress < 0.7)
             {
-                Control.ProgressTintColor = Color.FromHex("#B3316A").ToUIColor();
+                Control.ProgressDrawable.SetTint(Color.FromHex("#B3316A").ToAndroid());
             }
 
-            LayoutSubviews();
-        }
-
-        public override void LayoutSubviews()
-        {
-            base.LayoutSubviews();
-
-            float x = 1.0f;
-            float y = 4.0f;
-
-            CGAffineTransform transform = CGAffineTransform.MakeScale(x, y);
-            Transform = transform;
+            Control.ScaleY = 3.0f;
         }
     }
 }
