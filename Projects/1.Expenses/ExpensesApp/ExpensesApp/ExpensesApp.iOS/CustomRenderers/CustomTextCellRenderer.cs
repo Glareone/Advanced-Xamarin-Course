015cde61b0e3af1a44b9fa6a07e867d33b7e5d76
Glareone <enlarius@gmail.com>
2019-12-11 13:39:32 +0300

    Custom Text Renderer IOS

---
+++ Projects/1.Expenses/ExpensesApp/ExpensesApp/ExpensesApp.iOS/CustomRenderers/CustomTextCellRenderer.cs
@@ -0,0 +1,42 @@
+﻿using ExpensesApp.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+// Necessary attribute
+[assembly: ExportRenderer(typeof(TextCell), typeof(CustomTextCellRenderer))]
+
+namespace ExpensesApp.iOS.CustomRenderers
+{
+    // Customize Text cells from ExpensesPage (add right side control to each cell)
+    public class CustomTextCellRenderer : TextCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+
+            // based on styleId which could be defined directly in ExpensePage.xaml
+            switch (item.StyleId)
+            {
+                case "none":
+                    cell.Accessory = UITableViewCellAccessory.None;
+                    break;
+                case "checkmark":
+                    cell.Accessory = UITableViewCellAccessory.Checkmark;
+                    break;
+                case "detail-button":
+                    cell.Accessory = UITableViewCellAccessory.DetailButton;
+                    break;
+                case "detail-disclosure-button":
+                    cell.Accessory = UITableViewCellAccessory.DetailDisclosureButton;
+                    break;
+                case "disclosure":
+                default:
+                    cell.Accessory = UITableViewCellAccessory.DisclosureIndicator;
+                    break;
+            }
+
+            return cell;
+        }
+    }
+}
