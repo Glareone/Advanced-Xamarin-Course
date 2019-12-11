110d8bcbf438e61cc06514acd298a478d19a19be
Glareone <enlarius@gmail.com>
2019-12-11 13:49:41 +0300

    android custom renderer

---
+++ Projects/1.Expenses/ExpensesApp/ExpensesApp/ExpensesApp.Android/CustomRenderers/CustomTextCellRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Android.Content;
+using Android.Views;
+using ExpensesApp.iOS.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using View = Android.Views.View;
+
+// Necessary attribute
+[assembly: ExportRenderer(typeof(TextCell), typeof(CustomTextCellRenderer))]
+
+namespace ExpensesApp.iOS.CustomRenderers
+{
+    // Customize Text cells from ExpensesPage (instead of Ios where we added a new control to right side of each cell - here we only changed a color)
+    // for demo purposes, cell contains a lot of SetXX abilities
+    public class CustomTextCellRenderer : TextCellRenderer
+    {
+        protected override View GetCellCore(Cell item, View convertView, ViewGroup parent, Context context)
+        {
+            var cell = base.GetCellCore(item, convertView, parent, context);
+
+            // based on styleId which could be defined directly in ExpensePage.xaml
+            switch (item.StyleId)
+            {
+                case "none":
+                    cell.SetBackgroundColor(Android.Graphics.Color.AliceBlue);
+                    break;
+                case "checkmark":
+                    cell.SetBackgroundColor(Android.Graphics.Color.Aqua);
+                    break;
+                case "detail-button":
+                    cell.SetBackgroundColor(Android.Graphics.Color.Beige);
+                    break;
+                case "detail-disclosure-button":
+                    cell.SetBackgroundColor(Android.Graphics.Color.Chartreuse);
+                    break;
+                case "disclosure":
+                default:
+                    cell.SetBackgroundColor(Android.Graphics.Color.DarkKhaki);
+                    break;
+            }
+
+            return cell;
+        }
+    }
+}
